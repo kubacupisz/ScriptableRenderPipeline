ee475c9fa5f9c4a84c8ccf53124a35762374815a
Torbjorn Laedre <271210+tlaedre@users.noreply.github.com>
2019-08-22 18:23:13 +0200

    Invite some friends..

--- com.unity.render-pipelines.high-definition/Runtime/AssemblyInfo.cs
+++ com.unity.render-pipelines.high-definition/Runtime/AssemblyInfo.cs
@@ -1,6 +1,11 @@
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Editor")]
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Editor.Tests")]
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Runtime.Tests")]
 
+//custom-begin: Expose internal guts to dependencies
+[assembly: InternalsVisibleTo("Unity.DemoTeam.Playables")]
+[assembly: InternalsVisibleTo("PropertyMaster")]
+[assembly: InternalsVisibleTo("Unity.Demo.VFXLookDev")]
+//custom-end:
